5cff51aaa5f864e725bf68c9b0cbf5cc9c0ea409
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-08-28 21:55:40 +0530

    feat: Added the make file to compile the text editors. Also wrote code to read user input 1byte at a time until q is entered. Trying to get the terminal into raw mode.

--- textEditor.c
+++ textEditor.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+#include<unistd.h>
 int main() {
-    printf("Hello!\n");
+    // Recording the key presses from the user.
+    char c;
+    // read 1 byte from the standard input and store it in the c variable and continue till we get
+    // inputs to read.
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
