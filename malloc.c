d9d5baaec03a02b050df082b070da42ab1b2821d
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-10-30 17:20:05 +0530

    feat: Wrote a socket code that fetches response from a server through the HTTP protocol from Google IP address. Paused with the build of malloc.

--- malloc.c
+++ malloc.c
@@ -5,6 +5,7 @@
 #include<stdbool.h>
 #define HEAP_CAP 640000
 #define HEAP_ALLOCED_CAP 1024
+#define HEAP_FREED_CAP 1024
 typedef struct {
     void *start;
     size_t size;
@@ -14,26 +15,48 @@ char heap[HEAP_CAP] = {0};
 Heap_chunk heap_alloced[HEAP_ALLOCED_CAP] = {0}; // This is sort of a metadata table.
 size_t heap_size = 0;
 size_t heap_alloced_size = 0;
+Heap_chunk heap_freed[HEAP_FREED_CAP] = {0};
 
 void *heap_alloc(size_t size) {
-    assert(heap_size + size <= HEAP_CAP);
-    void *result = heap + heap_size;
-    heap_size += size;
+    if (size > 0) {
+        assert(heap_size + size <= HEAP_CAP);
+        void *result = heap + heap_size;
+        heap_size += size;
 
-    const Heap_chunk chunk = {
-        .start = result,
-        .size = size,
-    };
-    
-    assert(heap_alloced_size < HEAP_ALLOCED_CAP);
-    heap_alloced[heap_alloced_size++] = chunk;
+        const Heap_chunk chunk = {
+            .start = result,
+            .size = size,
+        };
+        
+        assert(heap_alloced_size < HEAP_ALLOCED_CAP);
+        heap_alloced[heap_alloced_size++] = chunk;
 
-    return result;
+        return result;
+    }
+    else {
+        return NULL;
+    }
 }
 
+// Function 
+void heap_dump_alloced_chunks(void) {
+    printf("Allocated chunks (%zu):\n", heap_alloced_size);
+    for (size_t i = 0; i < heap_alloced_size; i++) {
+        printf(" start: %p, size: %zu\n", 
+               heap_alloced[i].start, 
+               heap_alloced[i].size);
+    }
+}
+
+// TC O(alloced_size)
 void heap_free(void *ptr) {
-    (void) ptr;
-    assert(false && "Not implemented yet");
+
+    for (size_t i = 0; i < heap_alloced_size; i++) {
+        if (heap_alloced[i].start == ptr) {
+            
+        }
+    }
+
 }
 
 void heap_collect() {
@@ -41,12 +64,16 @@ void heap_collect() {
 }
 
 int main() {
-    char *root = heap_alloc(26);
-    for (int i = 0; i < 26; i++) {
-        root[i] = i + 'A';
+    for (int i = 0; i < 100; i++) {
+        void *p = heap_alloc(i);
+        if (i%2 == 0) {
+            heap_free(p); // try to do fragmentation.
+        }
     }
 
-    heap_free(root);
+    heap_dump_alloced_chunks();
+
+    //heap_free(root);
 
     return 0;
 }
